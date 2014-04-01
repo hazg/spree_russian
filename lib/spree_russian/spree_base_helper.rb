208daa72bc0c9a0125a4187cbb39a1c1a2cf4c85
hazg <hazg@mail.ru>
2014-04-01 14:26:56 +0400

    tuneup

--- lib/spree_russian/spree_base_helper.rb
+++ lib/spree_russian/spree_base_helper.rb
@@ -1,18 +1,8 @@
 module Spree
   BaseHelper.module_eval do
+    alias_method :original_available_countries, :available_countries
     def available_countries
-      checkout_zone = Zone.find_by(name: Spree::Config[:checkout_zone])
-
-      if checkout_zone && checkout_zone.kind == 'country'
-        countries = checkout_zone.country_list
-      else
-        countries = Country.all
-      end
-
-      countries.collect do |country|
-        country.name = Spree.t(country.iso, scope: 'country_names', default: country.name)
-        country
-      end.sort { |a, b| a.name <=> b.name }
+      original_available_countries.sort { |a, b| a.name <=> b.name }
     end
   end
 end
